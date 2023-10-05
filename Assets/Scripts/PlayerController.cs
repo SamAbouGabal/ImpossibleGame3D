ac8fdef3fcd650a807c9dd67952d3ee920114250
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-05 10:20:03 +0200

    Made the Player jump
    When pressing "Space" it adds force to Xaxis to designated gameObject  Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.04f; //Field
+    
     
     
     void Start()
@@ -14,13 +13,13 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Input isKeyWasJustPressed = Input.GetKeyDown(KeyCode.Space);
-        if (...)
-        {
-            
-        }
-        Rigidbody rb = GetComponent<Rigidbody>();
-        
         transform.Translate(0, 0, speed);
+        bool whenKeyIsPressed = Input.GetKeyDown(KeyCode.Space);
+        if (whenKeyIsPressed)
+        {
+            Rigidbody rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForce(0, 300, 0);
+        }
+        
     }
 }
