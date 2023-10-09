fc6d6c4c12c9b589bc6080d3982be66210224ccf
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2023-10-09 11:29:59 +0200

    Added if Null to followPlayer script
    so it doesn't give null message when player gets destroyed and camera dont have a game object to "Find"

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,6 +8,10 @@ using UnityEngine;
         private void LateUpdate()
         {
             GameObject player = GameObject.Find("Player");
+            if (player == null)
+            {
+                return;
+            }
             Vector3 target = player.transform.position + new Vector3(0, 1.4f, -5f);
             transform.position = target;
         }
